f583479e3708be84e095e5248c79deb723c15764
Stéphane <contact@stephane-richin.fr>
2020-07-10 15:46:10 +0200

    :tada: Add TypeScript and replace yarn by pnpm

---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+const Home = () => (
+  <div>
+    <header>
+      <div>logo</div>
+      <nav>
+        <a href="">why</a>
+        <a href="">download</a>
+      </nav>
+    </header>
+    <main>
+      <section>
+        <h1>Youtube Audio Player</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi sint
+          in officiis a placeat beatae ea atque et odit, tenetur libero aliquam.
+          Nemo officiis odit repellendus assumenda quidem. Facere, corporis.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque error
+          ex molestias tempore nesciunt explicabo libero perferendis voluptates
+          provident! Id facere, eaque consequatur fugiat cum soluta inventore
+          reprehenderit veniam numquam nobis qui laudantium nesciunt itaque esse
+          pariatur eligendi sint mollitia incidunt aspernatur tenetur. Corporis,
+          quia! Tempore est excepturi impedit molestias ipsam voluptatibus nisi
+          debitis fugit fugiat, libero in voluptas voluptatem, nobis itaque
+          minima, fuga quidem? Natus quaerat omnis quae saepe. Cum odit
+          dignissimos velit explicabo aperiam! Laudantium earum unde accusantium
+          aperiam consectetur eos? Voluptatem, sit! Odio fugiat nulla dolorem
+          deserunt perspiciatis repellat id aspernatur tenetur dolorum,
+          voluptates enim, quaerat modi!
+        </p>
+        <a>download</a>
+      </section>
+      <aside>img</aside>
+    </main>
+  </div>
+);
+
+export default Home;
